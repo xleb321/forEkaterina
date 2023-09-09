0dd3d174e91eea5d6b73b4f72d43f8e6a3c73d88
VillainAi <101522874+VillainAi@users.noreply.github.com>
2023-09-09 21:05:11 +0300

    NeBusy

--- kaisa/components/main.js
+++ kaisa/components/main.js
@@ -7,3 +7,34 @@ document.querySelectorAll('.container_ span').forEach((elem) => {
 	});
 });
 
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const sliders = Array.from(slider.querySelectorAll('img'));
+const slideCount = sliders.length;
+let slideIndex = 0;
+
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+function showPreviousSlide() {
+	slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+	updateSlider();
+}
+
+function showNextSlide() {
+	slideIndex = (slideIndex + 1) % slideCount;
+	updateSlider();
+}
+
+function updateSlider() {
+	sliders.forEach((slide, index) => {
+		if (index === slideIndex) {
+			slide.style.display = 'block';
+		} else {
+			slide.style.display = 'none';
+		}
+	});
+}
+
+updateSlider();
