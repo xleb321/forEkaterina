8a8c22ac3f02100dc2ab568c960f496c754b5643
xleb <92212079+xleb321@users.noreply.github.com>
2023-10-11 07:50:35 +0300

    save 11.10.23

--- hleb/components/main.js
+++ hleb/components/main.js
@@ -1,5 +1,45 @@
 import F_slider from './slider/script.js';
 
+function clear() {
+	for (let i = 1; i < document.querySelector('.content').children.length; i++) {
+		document.querySelector('.content').children[i].remove();
+	}
+}
+
+
+function headContacts(obg) {
+	obg.innerHTML += /*html*/ `
+		<section class='contactHead' data-speed="0.85" data-lag="0.5">
+			Контакты и информация
+		</section>
+	`;
+}
+
+function infoContacts(obg) {
+	obg.innerHTML += /*html*/ `
+		<section class='contactInfo' data-speed="0.9" data-lag="0.5">
+			<div>
+				<div class='head'>Телефон</div>
+				<div class='info'>+7 (928) 609-51-10</div>
+			</div>
+			<div>
+				<div class='head'>Почта</div>
+				<div class='info'>mirkoles61@yandex.ru</div>
+			</div>
+			<div>
+				<div class='head'>Адрес</div>
+				<div class='info'>г. Ростов-на-Дону,
+				Днепропетровская, 54</div>
+			</div>
+			<div>
+				<div class='head'>График работы</div>
+				<div class='info'>Ежедневно: 09:00 - 18:00</div>
+			</div>
+		</section>
+	`;
+}
+
+
 // header
 document.querySelectorAll('header .botLne span').forEach((elem) => {
 	elem.addEventListener('click', (e) => {
@@ -8,13 +48,6 @@ document.querySelectorAll('header .botLne span').forEach((elem) => {
 		});
 		e.target.classList.add('accent');
 	});
-});
-
-// slider
-document.querySelector('#slider').addEventListener('click', (e) => {
-	if (e.target.id == 'btn') {
-		F_slider(e.target);
-	}
 });
 
 // GSAP
@@ -28,3 +61,9 @@ if (ScrollTrigger.isTouch !== 1) {
 		effects: true,
 	});
 }
+
+document.querySelector('#slider').addEventListener('click', (e) => {
+	if (e.target.id == 'btn') {
+		F_slider(e.target);
+	}
+});
